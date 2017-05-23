90e9d27662ccf7ca72a9e1047e0b3fa797da1309
grek-andrian <gretsko.andrian@gmail.com>
2017-05-24 02:20:44 +0300

    Output list of student

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let crudApp = angular.module("crudApp", []);
+
+crudApp.controller("studentsController", function studentsController(){
+
+  this.newStudent = {};
+  this.saveStudent = function(){
+    studentsList.push(newStudent);
+    this.newStudent = {};
+  };
+
+  this.studentsList = [
+    {firstname: "Ivan" , lastname: "Franko", email: "i.franko@ukr.net"},
+    {firstname: "Taras" , lastname: "Shevchenko", email: "t.shevchenko@ukr.net"},
+    {firstname: "Lesya" , lastname: "Ukrainka", email: "l.ukrainka@ukr.net"}
+  ];
+});
