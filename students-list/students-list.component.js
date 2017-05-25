d7765fdfaeb90b032dfed5c2397e27ccef361051
grek-andrian <gretsko.andrian@gmail.com>
2017-05-25 15:33:52 +0300

    Add ngRoute

---
+++ students-list/students-list.component.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.
+module('studentsList').
+component('studentsList', {
+    templateUrl: 'students-list/students-list.template.html',
+    controller: ['studentsController',
+        function studentsController($firebaseArray) {
+            var ref = firebase.database().ref().child("students");
+            this.studentsList = $firebaseArray(ref);
+
+            this.clickedStudent = {};
+
+            this.message = "";
+
+            this.clearMessage = function () {
+                this.message = "";
+            };
+
+            this.selectStudent = function (student) {
+                this.clickedStudent = student;
+            };
+
+            this.updateStudent = function () {
+                var record = this.studentsList.$getRecord(this.clickedStudent.$id);
+                record.firstname = this.clickedStudent.firstname;
+                record.lastname = this.clickedStudent.lastname;
+                record.email = this.clickedStudent.email;
+
+                this.studentsList.$save(record).then(function (ref) {
+                    console.log("edited record with id " + ref.key);
+                });
+                this.message = "The student successfully updated";
+            };
+
+            this.deleteStudent = function () {
+                this.studentsList.$remove(this.clickedStudent);
+                this.message = "The student successfully deleted";
+            };
+
+            this.saveStudent = function () {
+                this.studentsList.$add({
+                    firstname: this.newStudent.firstname,
+                    lastname: this.newStudent.lastname,
+                    email: this.newStudent.email
+                }).then(function (ref) {
+                    console.log("added record with id " + ref.key);
+                });
+                this.newStudent = {};
+                this.message = "The new student successfully added";
+            };
+        }
+    ]
+});
